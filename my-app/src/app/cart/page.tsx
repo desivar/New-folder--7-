d967879a52d430906b040d584c2a1f71204d84f9
desivar <jillian_desire@yahoo.es>
2025-08-20 17:34:15 -0600

    cart/page.tsx

---
+++ my-app/src/app/cart/page.tsx
@@ -0,0 +1,8 @@
+'use client'; // Needed if you want to use hooks or state inside this page
+
+import React from 'react';
+import Cart from '@/components/Cart/Cart'; // Assuming you put your Cart component here
+
+export default function CartPage() {
+  return <Cart />;
+}
